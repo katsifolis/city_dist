8d4cad4c7bc292e2df084280423fba43847ca108
oxilgk <vazkats@gmail.com>
2020-09-07 16:34:42 +0300

    Formula for distance ready. Json seems to have incorrect lat, lon values

--- distance.v
+++ distance.v
@@ -1,5 +1,7 @@
 import os
 import json
+import cli { Command, Flag }
+import math
 
 const (
 	flname = './cities5000.txt'
@@ -15,47 +17,95 @@ struct City {
 
 // read_cities constructs an array of all cities containng the name, lat, lon of them
 fn read_cities(file_name string) int {
-
 	f := os.read_lines(file_name) or {
 		panic(err)
 	}
-
 	mut w := os.create('co.json') or {
 		panic('Cannot open or create file')
 		return -1
 	}
-
-
 	w.write('[ ')
-	
-	for n, city in f {
-
+	for city in f {
 		c := city.split('\t')
 		country := c[8].str()
 		name := c[1].str().replace('"', '').replace('"', '')
-		lat  := c[4].str()
-		lon  := c[5].str()
+		lat := c[4].str()
+		lon := c[5].str()
 		b := '{ "country" : "$country", "name": "$name", "lat" : "$lat", "lon" : "$lon" },'
-
 		w.write(b)
-
 	}
 	w.write(']')
-
 	w.close()
 	return 0
 }
 
-fn main() {
-	// This is for reading the files and constructing the json file
-	//	_ := read_cities(flname)
-	//	println('Loading cities ..')
+fn deg2rad(deg f64) f64 {
+	return deg * (math.pi / 180)
+}
 
-	f := os.read_file('./cities.json') or {
-		panic("Couldn't open file")
+fn calculate_distance(lat1, lon1, lat2, lon2 f64) f64 {
+
+	r := 6371 // Radius of earth in km
+	d_lat := deg2rad(lat2-lat1)  // deg2rad below
+	d_lon := deg2rad(lon2-lon1) 
+	a := math.sin(d_lat/2) * math.sin(d_lat/2) + math.cos(deg2rad(lat1)) * math.cos(deg2rad(lat2)) * math.sin(d_lon/2) * math.sin(d_lon/2)
+
+	c := 2 * math.atan2(math.sqrt(a), math.sqrt(1-a)) 
+	d := r * c // Distance in km
+
+	return d
+
+}
+
+
+fn main() {
+
+	if os.args.len != 3 {
+		println('usage: distance town1 town2')
 		return
 	}
 
-	s := json.decode([]City, f) ?
+	f := os.read_file('./cities.json') or {
+		panic(err)
+		return
+	}
 
+	cities := json.decode([]City, f) or {
+		panic(err)
+		return
+	}
+
+
+	mut lat1, mut lon1, mut lat2, mut lon2 := f64(0), f64(0), f64(0), f64(0)
+	mut flag, mut flag1 := true, true
+	from, to :=  os.args[1], os.args[2]
+	for city in cities {
+		if from.capitalize() == city.name && flag == true {
+			println(city.lat)
+			println(city.lon)
+			lat1 = city.lat.f64()
+			lon1 = city.lon.f64()
+			flag = false
+			
+		}
+		if to.capitalize() == city.name && flag1 == true{
+			print(city.name)
+			lat2 = city.lat.f64()
+			lon2 = city.lon.f64()
+			flag1 = false
+		}
+
+		if flag == false && flag1 == false {
+			print('hi')
+			break
+		}
+	}
+
+	d := calculate_distance(lat1, lon1, lat2, lon2)
+
+	print(" Distance between $from -> $to is $d km \n")
+
+	// This is for reading the files and constructing the json file
+	// _ := read_cities(flname)
+	// println('Loading cities ..')
 }
